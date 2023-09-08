ec7904eb5323d89c477ad2ed7b30f7c34643b760
agloveduck <60003839+agloveduck@users.noreply.github.com>
2023-09-08 13:30:52 +0800

    0908_ver1
    train完model

--- model.py
+++ model.py
@@ -14,21 +14,22 @@ class Model(nn.Module):  # 定义模型
             bidirectional=True,  # 设置双向
         )  # lstm层
         self.linear = nn.Linear(2 * HIDDEN_SIZE, TARGET_SIZE)  # linear层 双向所以×2
-        # self.crf = CRF(TARGET_SIZE, batch_first=True)
+        self.crf = CRF(TARGET_SIZE, batch_first=True)
 
     def _get_lstm_feature(self, input):
         out = self.embed(input)
         out, _ = self.lstm(out)
         return self.linear(out)
 
-    def forward(self, input, mask):
+    def forward(self, input, mask):  #  返回的是一个tensor结构
         out = self._get_lstm_feature(input)
-        return out
-        # return self.crf.decode(out, mask)
+        # return out
+        return self.crf.decode(out, mask)
+        # return self.crf._viterbi_decode(out, mask)
 
-    # def loss_fn(self, input, target, mask):
-    #     y_pred = self._get_lstm_feature(input)
-    #     return -self.crf.forward(y_pred, target, mask, reduction='mean')
+    def loss_fn(self, input, target, mask):  # 损失函数定义
+        y_pred = self._get_lstm_feature(input)
+        return -self.crf.forward(y_pred, target, mask, reduction='mean')  # 每一个batch算一个平均损失
 
 if __name__ == '__main__':
     model = Model()
--- predict.py
+++ predict.py
@@ -3,18 +3,45 @@ from model import *
 from config import *
 
 if __name__ == '__main__':
-    text = '每个糖尿病患者,无论是病情轻重,不论是注射胰岛素,还是口服降糖药,都必须合理地控制饮食。'
-    _, word2id = get_vocab()
-    input = torch.tensor([[word2id.get(w, WORD_UNK_ID) for w in text]])
-    mask = torch.tensor([[1] * len(text)]).bool()
+    # text = '每个糖尿病患者,无论是病情轻重,不论是注射胰岛素,还是口服降糖药,都必须合理地控制饮食。'
+    # _, word2id = get_vocab()
+    # input = torch.tensor([[word2id.get(w, WORD_UNK_ID) for w in text]])
+    # mask = torch.tensor([[1] * len(text)]).bool()
+    #
+    # model = torch.load(MODEL_DIR + 'model_40.pth', map_location=DEVICE)
+    # y_pred = model(input, mask)
+    # id2label, _ = get_label()
+    #
+    # label = [id2label[l] for l in y_pred[0]]
+    # print(text)
+    # print(label)
+    #
+    # info = extract(label, text)
+    # print(info)
+    dataset = Dataset()
+    loader = data.DataLoader(
+        dataset,
+        batch_size=100,
+        shuffle=True,
+        collate_fn=collate_fn,
+    )
 
-    model = torch.load(MODEL_DIR + 'model_40.pth', map_location=DEVICE)
-    y_pred = model(input, mask)
-    id2label, _ = get_label()
+    model = Model()
+    optimizer = torch.optim.Adam(model.parameters(),lr=LR)
 
-    label = [id2label[l] for l in y_pred[0]]
-    print(text)
-    print(label)
+    for e in range(EPOCH):
+        for b,(input,target,mask) in enumerate(loader):
+            y_pred = model(input,mask)
+            loss = model.loss_fn(input,target,mask)
 
-    info = extract(label, text)
-    print(info)
+            optimizer.zero_grad()  # 梯度清零
+            loss.backward()  # 反向传播
+            optimizer.step()  # 优化器迭代
+            # 三步走完应该有一个损失下降的过程
+
+            if b % 10 == 0:
+                print('>>epoch:',e,'loss:',loss.item())  # 训练过程中只要loss是在下降就行
+
+            torch.save(model,MODEL_DIR+f'model_{e}.pth')
+            # print(y_pred)  # 二维list
+            # exit()
